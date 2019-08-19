8dadcfbdc753346c81b70fe07f4ddbae16df217e
AadithyaVenkat <aadithyavenkat@gmail.com>
2019-08-18 23:14:23 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
+  title: 'Travel Blog', // Required
+  author: 'Aadithya Venkatanarayanan', // Required
+  description: 'Photographer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
